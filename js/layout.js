8e01c2717b5b98c8b3eba790d6adc4017be27bf2
minforever557-netizen <minforever557@gmail.com>
2026-02-17 11:49:22 +0700

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -2,3 +2,15 @@ function toggleSidebar(){
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("hidden");
 }
+// โหลด Sidebar
+async function loadLayout() {
+
+  const sidebar = await fetch("components/sidebar.html");
+  const sidebarHTML = await sidebar.text();
+
+  document.getElementById("sidebar").innerHTML = sidebarHTML;
+
+}
+
+loadLayout();
+
